ae3cd29399ca5dc0bc568dc4c3c68359a86dc106
Sam Lachance <slachance@gmail.com>
2015-11-15 15:36:08 -0500

    Refactored Call class

--- 911-tracker.rb
+++ 911-tracker.rb
@@ -1,56 +1,57 @@
 require "nokogiri"
 require "open-uri"
 
+
 class Call
-  attr_accessor :status, :agency, :location, :query_status, :query_agency, :query_location
+  POLL_TIME = ENV['POLL_TIME'] || 60
+
+  attr_accessor :id, :status, :agency, :location
+  @@previous_ids = []
+
+  def initialize(status, agency, location)
+    self.id = status.split(", ")[0]
+    self.status = status
+    self.agency = agency
+    self.location = location
+  end
   
-  def initialize  
-    @call_data = Nokogiri::HTML(open("http://hamilton911.discoveregov.com/ajax.php?ts=")); nil
-    @status = @call_data.css('td')[0].text
-    @agency = @call_data.css('td')[1].text
-    @location = @call_data.css('td')[2].text.strip
-    @location.slice! ("View on Map")
+  def print
+    puts status
+    puts agency
+    puts location
   end
 
-  # Asks the server for its most recent call and then compares it to your record
-  def query
-    @query_data = Nokogiri::HTML(open("http://hamilton911.discoveregov.com/ajax.php?ts=")); nil
-    @query_status = @query_data.css('td')[0].text
-    @query_agency = @query_data.css('td')[1].text
-    @query_location = @query_data.css('td')[2].text.strip
-    @query_location.slice! ("View on Map")
+  def self.fetch
+    Nokogiri::HTML(open("http://hamilton911.discoveregov.com/ajax.php?ts="))
+  end
+  
+  def self.parse
+    call_data = fetch
+    call_data.css('br').each{ |br| br.replace ", " }
+    call_data.css("tbody tr").map do |tr|
+      status = tr.css('td')[0].text
+      agency = tr.css('td')[1].text
+      location = tr.css('td')[2].text.strip
+      location.slice! ("View on Map")
+      Call.new(status, agency, location)
+    end.reverse
+  end
 
-    unless @query_status == @status
-      self.update
+  def self.latest
+    calls = parse
+    new_ids = calls.map(&:id)
+    calls.delete_if {|call| @@previous_ids.include? call.id}
+    @@previous_ids = new_ids
+    calls
+  end
+
+  def self.listen
+    loop do
+      Call.latest.each(&:print)
+      sleep (POLL_TIME.to_i)
     end
-  end
-  
-  # Updates the current call information with the information that was obtained from the server
-  def update
-    @status = @query_status
-    @agency = @query_agency
-    @location = @query_location
-
-    puts @status
-    puts @agency
-    puts @location
   end
 end
 
 puts "Welcome to Status"
-print "Please enter refresh rate (in seconds): "
-refresh_rate = gets
-
-# Creates object from the Call class
-call = Call.new
-puts "Fetching current call..."
-# Prints the current call information that was obtained when the call object was created
-puts call.status
-puts call.agency
-puts call.location
-
-# Checks the server every x seconds for new information
-loop do
-  call.query
-  sleep (refresh_rate.to_i)
-end
+Call.listen
